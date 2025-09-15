c4c4aec20b391b475b6309eb5a8ba3e0eb3e2388
Subhodip Ghosh <macy16661@gmail.com>
2025-09-16 02:09:05 +0530

    Implemented dijkastras pq algo

--- src/PathWeaver/PathWeaver.hpp
+++ src/PathWeaver/PathWeaver.hpp
@@ -107,6 +107,62 @@ class BFS: public PathFinder {
         ~BFS() override = default;
 };
 
+class DijkastrasPQ: public PathFinder {
+    public:
+        /*
+            Priority queue base implementation of dijkastra's algorithm
+        */
+        void findPath(std::vector<int>& currPos, std::vector<int>& goal, std::vector<std::pair<int, int>>& plannedPath, std::vector<std::vector<int>>& maze) override {
+            int n = maze.size(), m = maze[0].size();
+        int sr = currPos[0], sc = currPos[1];
+        int er = goal[0], ec = goal[1];
+
+        // Min-heap: {dist, row, col}
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        vector<vector<pair<int,int>>> parent(n, vector<pair<int,int>>(m, {-1,-1}));
+
+        dist[sr][sc] = 0;
+        pq.push({0, sr, sc});
+
+        // 8 directions
+        vector<int> dr = {-1,-1,-1, 0, 0, 1, 1, 1};
+        vector<int> dc = {-1, 0, 1,-1, 1,-1, 0, 1};
+
+        while (!pq.empty()) {
+            auto node = pq.top(); pq.pop();
+            int curDist = node[0], r = node[1], c = node[2];
+
+            if (r == er && c == ec) break; // reached goal
+
+            for (int k = 0; k < 8; k++) {
+                int nr = r + dr[k], nc = c + dc[k];
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m && maze[nr][nc] == 1) {
+                    if (dist[r][c] + 1 < dist[nr][nc]) {
+                        dist[nr][nc] = dist[r][c] + 1;
+                        parent[nr][nc] = {r, c};
+                        pq.push({dist[nr][nc], nr, nc});
+                    }
+                }
+            }
+        }
+
+        // reconstruct path
+        plannedPath.clear();
+        if (dist[er][ec] == INT_MAX) return; // no path
+
+        int r = er, c = ec;
+        while (r != -1 && c != -1) {
+            plannedPath.push_back({r, c});
+            auto [pr, pc] = parent[r][c];
+            r = pr; c = pc;
+        }
+        reverse(plannedPath.begin(), plannedPath.end());
+    }
+
+        ~DijkastrasPQ() override = default;
+};
+
 class DFS: public PathFinder {
     public:
         void findPath(std::vector<int>& currPos, std::vector<int>& goal, std::vector<std::pair<int, int>>& plannedPath, std::vector<std::vector<int>>& maze) override {
